a57c0a9950e4c23d51bd4b79a7fd9ed2aad7854a
Jinwoo Shin <bluesky0255@gmail.com>
2019-06-23 12:04:36 +0900

    Add project skeletal stub

---
+++ src/views/Game.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Game view</h1>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Game extends Vue {
+
+}
+</script>
+
